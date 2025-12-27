3bee50f494b4a5e31a1e05c0b19c281e38857fb7
Melnikov Artem <arkabrik18@gmail.com>
2025-12-27 15:52:46 +0300

    Update classes

--- src/main/java/ru/melnikov/telegrambot/config/WebConfig.java
+++ src/main/java/ru/melnikov/telegrambot/config/WebConfig.java
@@ -2,18 +2,13 @@ package ru.melnikov.telegrambot.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -22,7 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addFormatterForFieldType(LocalDate.class, new org.springframework.format.datetime.standard.TemporalAccessorPrinter(DATE_FORMATTER), new org.springframework.format.datetime.standard.TemporalAccessorParser(DATE_FORMATTER));
-        registry.addFormatterForFieldType(LocalDateTime.class, new org.springframework.format.datetime.standard.TemporalAccessorPrinter(DATE_TIME_FORMATTER), new org.springframework.format.datetime.standard.TemporalAccessorParser(DATE_TIME_FORMATTER));
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setDateFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        registrar.setDateTimeFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        registrar.registerFormatters(registry);
     }
 }
